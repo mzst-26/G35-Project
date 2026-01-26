26e24c78f5e300fa173629807edc1eadc1369521
Jrtowers-prog <james.towers@students.plymouth.ac.uk>
2026-01-26 16:17:18 +0000

    updated main dashboard page to include payments component

--- app/company/dashboard/home/page.tsx
+++ app/company/dashboard/home/page.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { NavItem, SectionKey } from "@/types/dashboard";
 import RecruiterHome from "@/components/recruiter_dashboard/recruiterHome";
 import CreateJobHome from "@/components/recruiter_dashboard/create-job";
+import Payments from "@/components/recruiter_dashboard/payments";
 import { useRouter } from "next/navigation";
 
 
@@ -140,11 +141,7 @@ export default function CompanyDashboard() {
             />
           )}
 
-            {activeSection === 'payments' && (
-              <>
-                this is Payments
-              </>
-          )}
+            {activeSection === 'payments' && <Payments />}
 
             {activeSection === 'support' && (
               <>
