9ebbefcab053ca11ccda0eff7ec07c5c3351852e
Jrtowers-prog <james.towers@students.plymouth.ac.uk>
2026-02-05 11:49:50 +0000

    Added ticket creation form

---
+++ app/trade/support/new/page.tsx
@@ -0,0 +1,10 @@
+import TicketForm from "@/components/trade_dashboard/support/TicketForm";
+
+export default function TradeSupportNewPage() {
+  // Page-level composition only
+  return (
+    <main className="min-h-screen bg-slate-50 p-6">
+      <TicketForm />
+    </main>
+  );
+}
